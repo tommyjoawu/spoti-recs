5a1f8a6c4da060758a7269589b9a12792893b3de
Tommy <tj@automatonzone.com>
2023-06-25 22:21:08 -0500

    removed search by artist

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,29 +1,28 @@
 class StaticPageController < ApplicationController
     def home
-        @artist = RSpotify::Artist.search('Arctic Monkeys').first
-        @top_tracks = @artist.top_tracks(:US)
+        # @artist = RSpotify::Artist.search('Arctic Monkeys').first
+        # @top_tracks = @artist.top_tracks(:US)
         @categories = RSpotify::Category.list
         #Removed Top List option from position 0 of array
         @categories.shift
 
         #Remove categories that do not bring recommendations
-        i = 0
-        cats_without_recs = []
-        display_list = []
-        delete_at = []
+        # i = 0
+        # cats_without_recs = []
+        # display_list = []
+        # delete_at = []
 
         #Spotify doesn't give recommendations on these categories (10 September 2022)
-        no_recs = ["Regional Mexican", "Christian & Gospel", "EQUAL", "R&B", "Workout", "Wellness", "Decades", "Pride", "RADAR", "At Home", "API Heritage Month", "Focus", "Kids & Family", "Dance/Electronic"]
-        cats_total = @categories.count
-
-
-        @categories.delete_if { |cat| no_recs.include?(cat.name) }
+        # no_recs = ["Regional Mexican", "Christian & Gospel", "EQUAL", "R&B", "Workout", "Wellness", "Decades", "Pride", "RADAR", "At Home", "API Heritage Month", "Focus", "Kids & Family", "Dance/Electronic"]
+        # no_recs = []
+        # cats_total = @categories.count
+        # @categories.delete_if { |cat| no_recs.include?(cat.name) }
         
         #Filter
         @random_category = @categories.second
-        recommendations = RSpotify::Recommendations.generate(seed_genres: [@random_category.name.downcase])
+        # recommendations = RSpotify::Recommendations.generate(seed_genres: [@random_category.name.downcase])
         
-        @random_track = recommendations.tracks.sample
+        # @random_track = recommendations.tracks.sample
     end
 
     def recommended_artists
